2f5f05483befa0939a52fc4a590eb0a7c5ad7e42
Himanshu Oli <himanshuoliofficial@gmail.com>
2024-04-06 09:30:06 +0530

    Added favicon.ico in middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,5 +5,10 @@ export default authMiddleware({
 });
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    "/", 
+    "/(api|trpc)(.*)",
+    "/favicon.ico" // Add the skipped URL to the matcher
+  ]
 };
